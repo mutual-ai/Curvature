7cdb3077b033f71b59c200eb5220d2c8243b14c4
Marcelo Souza <marcelo.mssouza@gmail.com>
2011-11-07 16:06:34 -0300

    Removed files we don't want to track Added .gitignore

--- saida/resultados.r
+++ saida/resultados.r
@@ -1,10 +1,12 @@
 file_list = dir("./",pattern = "dat$")
 err_rms = NULL
 mean = NULL
+sigma = NULL
 for (f in file_list) {
  aux <- read.table(f,header=TRUE)
  err_rms <- cbind(err_rms,aux$err_rms)
  mean <- cbind(mean,aux$mean)
 }
+sigma <- aux$sigma
 rm(aux,f)
 
